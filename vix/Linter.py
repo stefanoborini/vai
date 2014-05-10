49b67e9add70dedc8196e77c39be6574e439d763
Stefano Borini <stefano.borini@quantumwise.com>
2014-05-10 17:37:19 +0200

    Restored linting, although now it's slow as a crawl.

--- vix/Linter.py
+++ vix/Linter.py
@@ -19,53 +19,6 @@ class LinterResult:
         self.message = message
 
 
-class PyLintLinter:
-    def __init__(self):
-        pass
-
-    def lint(self, filename=None, original_filename=None, code=None):
-        env = os.environ
-        env['LC_ALL']='en_US.UTF-8'
-        env['LANG']='en_US.UTF-8'
-        stdout = tempfile.NamedTemporaryFile(delete=False)
-        stderr = tempfile.NamedTemporaryFile(delete=False)
-        raise Exception(filename)
-        proc = subprocess.Popen(['pylint',
-                           filename,
-                           "--msg-template='%s:{C}:{line}:{column}:{msg_id}:{obj}:{msg}'" % original_filename,
-                           '-r'
-                           'n',
-                           ],
-                           stdout = stdout,
-                           stderr = stderr,
-                           env=env,
-                           universal_newlines=True
-                           )
-        proc.wait()
-        stdout.close()
-        stderr.close()
-        with contextlib.closing(open(stdout.name)) as f:
-            lint_results = f.readlines()
-
-        f=open("xxx", "w")
-        f.write(str(lint_results))
-        f.close()
-        with contextlib.closing(open(stderr.name)) as f:
-            lint_results = f.readlines()
-
-        f=open("yyy", "w")
-        f.write(str(lint_results))
-        f.close()
-
-        result = []
-        for line in lint_results:
-            if line.startswith(original_filename):
-                info = line.split(':')
-                info = LinterResult(info[0], info[1], int(info[2]), int(info[3]), info[4], info[5], info[6])
-                result.append(info)
-        return result
-
-
 class Reporter:
     def __init__(self):
         self._errors = []
@@ -93,7 +46,48 @@ class Reporter:
         return self._errors
 
 class PyFlakesLinter:
-    def lint(self, filename=None, original_filename=None, code=None):
+    def lint(self, document):
         reporter = Reporter()
-        pyflakes.api.check(code, original_filename, reporter=reporter)
+        pyflakes.api.check(document.text(), document.filename(), reporter=reporter)
         return reporter.errors()
+
+class PyLintLinter:
+    def lint(self, document):
+        env = os.environ
+        env['LC_ALL']='en_US.UTF-8'
+        env['LANG']='en_US.UTF-8'
+        stdout = tempfile.NamedTemporaryFile(delete=False)
+        stderr = tempfile.NamedTemporaryFile(delete=False)
+        proc = subprocess.Popen(['pylint',
+                           document.filename(),
+                           "--msg-template='%s:{C}:{line}:{column}:{msg_id}:{obj}:{msg}'" % document.filename(),
+                           '-r'
+                           'n',
+                           ],
+                           stdout = stdout,
+                           stderr = stderr,
+                           env=env,
+                           universal_newlines=True
+                           )
+        proc.wait()
+        stdout.close()
+        stderr.close()
+        with contextlib.closing(open(stdout.name)) as f:
+            lint_results = f.readlines()
+
+        #with contextlib.closing(open(stderr.name)) as f:
+        #    lint_results = f.readlines()
+
+        result = []
+        for line in lint_results:
+            if line.startswith(document.filename()):
+                info = line.split(':')
+                info = LinterResult(filename=info[0],
+                                    level=info[1],
+                                    line=int(info[2]),
+                                    column=int(info[3]),
+                                    message=info[6]
+                                   )
+                result.append(info)
+        return result
+
