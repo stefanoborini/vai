00df879317310b134ddf731cf05c37e30200618e
Stefano Borini <stefano.borini@quantumwise.com>
2014-06-19 19:19:22 +0200

    Some nice options

--- vix/__init__.py
+++ vix/__init__.py
@@ -11,8 +11,11 @@ import argparse
 import cProfile
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="A Vim-like editor, with big dreams.")
     parser.add_argument("filename", nargs="?", help="The filename to open")
+    parser.add_argument("--profile", help="Enable profiling at the end of the run.", action="store_true")
+    parser.add_argument("--version", '-v', help="Print the version number.", action="version",
+                                           version='vix {0}'.format(__version__))
     args = parser.parse_args()
 
     try:
@@ -23,18 +26,25 @@ def main():
             e.openFile(args.filename)
 
         e.show()
-        pr = cProfile.Profile()
-        pr.enable()
+
+        if args.profile:
+            pr = cProfile.Profile()
+            pr.enable()
+
         app.exec_()
-        pr.disable()
-        s = io.StringIO()
-        sortby = 'tottime'
-        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-        ps.print_stats()
-        print(s.getvalue())
+
+        if args.profile:
+            pr.disable()
+            s = io.StringIO()
+            sortby = 'tottime'
+            ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+            ps.print_stats()
+            print(s.getvalue())
     except Exception as e:
         import traceback
-        open("crashreport.out", "w").write(traceback.format_exc())
+        import contextlib
+        with contextlib.closing(open("vix_crashreport.out", "w")) as f:
+            f.write(traceback.format_exc())
         pdb.post_mortem()
 
     return 0
