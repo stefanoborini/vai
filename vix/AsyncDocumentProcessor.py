4621c30b2f98403caa4d095354d18558ad167d76
Stefano Borini <stefano.borini@gmail.com>
2014-06-28 19:26:59 +0200

    Added exploratory async document processor

---
+++ vix/AsyncDocumentProcessor.py
@@ -0,0 +1,13 @@
+import process
+
+class AsyncDocumentProcessor:
+    def __init__(self, document):
+        self._document = document
+
+    def monitor(self, enabled):
+        raise NotImplementedError("AsyncDocumentProcess.monitor() must be implemented")
+
+    def runOnce(self):
+        raise NotImplementedError("AsyncDocumentProcess.runProcess() must be implemented")
+
+
