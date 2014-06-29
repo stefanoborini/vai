b964bab9c1db0ec1f8ddab0e04f1a09d73f32b69
Stefano Borini <stefano.borini@gmail.com>
2014-06-29 10:04:37 +0200

    Filed placeholder for bug reporting system

--- vix/BugReport.py
+++ vix/BugReport.py
@@ -1,2 +1,18 @@
 def report():
-    pass
+    print("Apologies. Vix has crashed. ")
+    print("A traceback report of the error has been created in vix_crashreport.log")
+
+    print("Would you like to send a bug report to the development team?")
+    print("The bug report will not contain information about your document content.")
+    if yes():
+        print("Sending traceback to developers")
+        print("Sending unsuccessful. Feature not implemented")
+
+
+def yes():
+    y_or_n = input("[y/N]> ")
+    if y_or_n.strip().lower() in ['y','yes']:
+        return True
+    return False
+
+
