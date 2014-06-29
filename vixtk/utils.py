e5ebc5d7f8636db5ffd3b64d8913ca5a0559536f
Stefano Borini <stefano.borini@gmail.com>
2014-06-29 14:37:28 +0200

    Added a test for clamp

--- vixtk/utils.py
+++ vixtk/utils.py
@@ -1,7 +1,14 @@
 
 def strformat(strings_at, length):
+    """
+    Returns a string of given length, composed by strings at specified
+    positions, as specified by strings_at. The rest is filled with spaces.
+    strings_at is a list of tuples of two elements, the first a number,
+    the second a string.
+    """
+
     res = ""
-    for pos, string in sorted(strings_at,key=lambda x: x[0]):
+    for pos, string in sorted(strings_at, key=lambda x: x[0]):
         res = res[:pos]
         res += " "* (pos - len(res))
         res += string
@@ -11,4 +18,8 @@ def strformat(strings_at, length):
     return res
 
 def clamp(value, minvalue, maxvalue):
+    """
+    Returns the value, clamped between min and max value
+    if outside that range
+    """
     return sorted([minvalue, value, maxvalue])[1]
