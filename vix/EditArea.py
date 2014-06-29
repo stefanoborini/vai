6c2dacc8d4dd40637e00488d8acdf95eccb1093a
Stefano Borini <stefano.borini@gmail.com>
2014-06-29 14:21:05 +0200

    21 Added coloring for strings and numerals

--- vix/EditArea.py
+++ vix/EditArea.py
@@ -8,10 +8,23 @@ from pygments import token
 
 TOKEN_TO_COLORS = {
     token.Keyword: (gui.VGlobalColor.yellow, None),
+    token.Keyword.Constant: (gui.VGlobalColor.red, None),
+    token.Keyword.Pseudo: (gui.VGlobalColor.red, None),
     token.Keyword.Namespace: (gui.VGlobalColor.magenta, None),
+    token.Keyword.Reserved: (gui.VGlobalColor.magenta, None),
+    token.Keyword.Type: (gui.VGlobalColor.magenta, None),
     token.Comment: (gui.VGlobalColor.cyan, None),
+    token.Comment.Single: (gui.VGlobalColor.cyan, None),
     token.Name.Function: (gui.VGlobalColor.cyan, None),
     token.Name.Class: (gui.VGlobalColor.cyan, None),
+    token.String:     (gui.VGlobalColor.red, None),
+    token.Literal:    (gui.VGlobalColor.red, None),
+    token.Number:     (gui.VGlobalColor.red, None),
+    token.Number.Integer:     (gui.VGlobalColor.red, None),
+    token.Number.Float:     (gui.VGlobalColor.red, None),
+    token.Number.Hex:     (gui.VGlobalColor.red, None),
+    token.Number.Oct:     (gui.VGlobalColor.red, None),
+    token.Operator.Word:    (gui.VGlobalColor.yellow, None),
 }
 
 class EditArea(gui.VWidget):
