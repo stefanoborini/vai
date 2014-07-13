11e4930d64b927f112010f286c0283613b95a597
Stefano Borini <stefano.borini@gmail.com>
2014-07-13 20:58:59 +0200

    Fixed a bug on VSignal

--- tests/vixtk/core/test_VSignal.py
+++ tests/vixtk/core/test_VSignal.py
@@ -15,3 +15,17 @@ class TestVSignal(unittest.TestCase):
         self.assertEqual(len(arg), 1)
         self.assertEqual(arg[0], 3)
 
+        signal.disconnect(slot)
+
+        signal.emit(3)
+
+        self.assertEqual(len(arg), 1)
+        self.assertEqual(arg[0], 3)
+
+    def testDisconnectNotRegistered(self):
+        arg = []
+        sender = core.VObject()
+        signal = core.VSignal(sender)
+        def slot(x):
+            arg.append(x)
+        signal.disconnect(slot)
