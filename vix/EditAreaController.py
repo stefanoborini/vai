b204bb4266857b05fc69ce1bcf707aec478564fd
Stefano Borini <stefano.borini@gmail.com>
2014-07-15 18:44:58 +0200

    Cleanup of the logic and delete character command

--- vix/EditAreaController.py
+++ vix/EditAreaController.py
@@ -49,16 +49,14 @@ class EditAreaController(core.VObject):
     # Private
 
     def _handleEventInsertMode(self, event):
+        command = None
+
         if event.key() == vixtk.Key.Key_Escape:
             self._editor_model.setMode(flags.COMMAND_MODE)
         elif event.key() == vixtk.Key.Key_Backspace:
             command = commands.DeleteSingleCharCommand(self._buffer)
-            self._buffer.commandHistory().append(command)
-            command.execute()
         elif event.key() == vixtk.Key.Key_Return:
             command = commands.BreakLineCommand(self._buffer)
-            self._buffer.commandHistory().append(command)
-            command.execute()
         else:
             if event.key() == vixtk.Key.Key_Tab:
                 text = " "*4
@@ -67,104 +65,121 @@ class EditAreaController(core.VObject):
 
             if len(text) != 0:
                 command = commands.InsertStringCommand(self._buffer, text)
-                self._buffer.commandHistory().append(command)
-                command.execute()
+
+        if command is not None:
+            self._buffer.commandHistory().append(command)
+            command.execute()
 
         event.accept()
 
     def _handleEventCommandMode(self, event):
+        # FIXME This code sucks. We need better handling of the state machine
+
+        # No commands. only movement and no-command operations
         if event.key() == vixtk.Key.Key_I:
             if event.modifiers() & vixtk.KeyModifier.ShiftModifier:
                 self._buffer.documentCursor().toLineBeginning()
             self._editor_model.setMode(flags.INSERT_MODE)
             event.accept()
+            return
 
-        elif event.key() == vixtk.Key.Key_X and event.modifiers() == 0:
-            self._buffer.documentCursor().deleteSingleCharAfter()
-            event.accept()
+        if event.key() == vixtk.Key.Key_G:
+            if event.modifiers() == 0:
+                self._editor_model.setMode(flags.GO_MODE)
+            elif event.modifiers() & vixtk.KeyModifier.ShiftModifier:
+                self._buffer.documentCursor().toLastLine()
+                self._buffer.editAreaModel().setDocumentPosAtTop((self._buffer.documentCursor().pos()[0]-self._edit_area.height()+1,1))
+                self._editor_model.setMode(flags.COMMAND_MODE)
 
-        elif event.key() == vixtk.Key.Key_G and event.modifiers() == 0:
-            self._editor_model.setMode(flags.GO_MODE)
             event.accept()
+            return
+
+        if event.key() == vixtk.Key.Key_D:
+            self._editor_model.setMode(flags.DELETE_MODE)
+            event.accept()
+            return
+
+        if event.key() == vixtk.Key.Key_Dollar:
+            self._edit_area.moveCursor(flags.END)
+            event.accept()
+            return
+
+        if event.key() == vixtk.Key.Key_AsciiCircum:
+            self._edit_area.moveCursor(flags.HOME)
+            event.accept()
+            return
+
+        if event.key() == vixtk.Key.Key_U:
+            if len(self._buffer.commandHistory()):
+                command = self._buffer.commandHistory().pop()
+                command.undo()
+            event.accept()
+            return
+
+        if event.key() == vixtk.Key.Key_A:
+            if event.modifiers() == 0:
+                self._editor_model.setMode(flags.INSERT_MODE)
+                self._edit_area.moveCursor(flags.LEFT)
+
+            elif event.modifiers() & vixtk.KeyModifier.ShiftModifier:
+                self._editor_model.setMode(flags.INSERT_MODE)
+                self._edit_area.moveCursor(flags.END)
+            else:
+                return
+            event.accept()
+            return
+
+        # Command operations
+        command = None
+
+        if event.key() == vixtk.Key.Key_X and event.modifiers() == 0:
+            command = commands.DeleteSingleCharAfterCommand(self._buffer)
 
         elif event.key() == vixtk.Key.Key_O:
             if event.modifiers() == 0:
                 self._editor_model.setMode(flags.INSERT_MODE)
                 command = commands.NewLineAfterCommand(self._buffer)
-                self._buffer.commandHistory().append(command)
-                command.execute()
-                event.accept()
-
             elif event.modifiers() & vixtk.KeyModifier.ShiftModifier:
                 self._editor_model.setMode(flags.INSERT_MODE)
                 command = commands.NewLineCommand(self._buffer)
-                self._buffer.commandHistory().append(command)
-                command.execute()
-                event.accept()
-            else:
-                return
-
-        elif event.key() == vixtk.Key.Key_A:
-            if event.modifiers() == 0:
-                self._editor_model.setMode(flags.INSERT_MODE)
-                self._edit_area.moveCursor(flags.LEFT)
-                event.accept()
-
-            elif event.modifiers() & vixtk.KeyModifier.ShiftModifier:
-                self._editor_model.setMode(flags.INSERT_MODE)
-                self._edit_area.moveCursor(flags.END)
-                event.accept()
-            else:
-                return
-
-        elif event.key() == vixtk.Key.Key_U:
-            if len(self._buffer.commandHistory()):
-                command = self._buffer.commandHistory().pop()
-                command.undo()
-            event.accept()
-
-        elif event.key() == vixtk.Key.Key_D:
-            self._editor_model.setMode(flags.DELETE_MODE)
-            event.accept()
-
-        elif event.key() == vixtk.Key.Key_G and event.modifiers() & vixtk.KeyModifier.ShiftModifier:
-            self._buffer.documentCursor().toLastLine()
-            self._buffer.editAreaModel().setDocumentPosAtTop((self._buffer.documentCursor().pos()[0]-self._edit_area.height()+1,1))
-            self._editor_model.setMode(flags.COMMAND_MODE)
-            event.accept()
-
         elif event.key() == vixtk.Key.Key_J and event.modifiers() & vixtk.KeyModifier.ShiftModifier:
-            self._buffer.documentCursor().joinWithNextLine()
-            event.accept()
-        elif event.key() == vixtk.Key.Key_Dollar:
-            self._edit_area.moveCursor(flags.END)
-            event.accept()
-        elif event.key() == vixtk.Key.Key_AsciiCircum:
-            self._edit_area.moveCursor(flags.HOME)
+            command = commands.JoinWithNextLineCommand(self._buffer)
+
+        if command is not None:
+            result = command.execute()
+            if result.success:
+                self._buffer.commandHistory().append(command)
             event.accept()
 
     def _handleEventDeleteMode(self, event):
         if event.key() == vixtk.Key.Key_Escape:
             self._editor_model.setMode(flags.COMMAND_MODE)
             event.accept()
-        elif event.key() == vixtk.Key.Key_D:
-            self._editor_model.setMode(flags.DELETE_MODE)
+            return
+
+        if event.key() == vixtk.Key.Key_D:
             command = commands.DeleteLineAtCursorCommand(self._buffer)
             result = command.execute()
             if result.success:
                 self._buffer.commandHistory().append(command)
             self._editor_model.setMode(flags.COMMAND_MODE)
             event.accept()
+            return
 
     def _handleEventGoMode(self, event):
         if event.key() == vixtk.Key.Key_Escape:
             self._editor_model.setMode(flags.COMMAND_MODE)
             event.accept()
-        elif event.key() == vixtk.Key.Key_G:
+            return
+
+        if event.key() == vixtk.Key.Key_G:
             self._buffer.editAreaModel().setDocumentPosAtTop((1,1))
             self._buffer.documentCursor().toFirstLine()
             self._editor_model.setMode(flags.COMMAND_MODE)
             event.accept()
+            return
+
+    # Private
 
     def _hasModels(self):
         return self._buffer and self._editor_model
