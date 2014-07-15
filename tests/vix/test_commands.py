e53470bb7267eae5aa373d96943a52e8b2ce991a
Stefano Borini <stefano.borini@gmail.com>
2014-07-15 21:54:37 +0200

    Added test for command, and module for handling a fixture

--- tests/vix/test_commands.py
+++ tests/vix/test_commands.py
@@ -1,17 +1,32 @@
 import unittest
 from vix.models.Buffer import Buffer
-from vix.models.TextDocument import TextDocument
+from vix.models.TextDocument import TextDocument, LineMeta
 from vix.models.EditAreaModel import EditAreaModel
 from vix import commands
+from tests import fixtures
+
 
 class TestCommands(unittest.TestCase):
     def setUp(self):
-        document = TextDocument()
+
+        document = TextDocument(fixtures.get("basic_python.py"))
         edit_area_model = EditAreaModel()
         self.buffer = Buffer(document, edit_area_model)
 
     def testNewLineCommand(self):
         command = commands.NewLineCommand(self.buffer)
+        status = command.execute()
+        self.assertNotEqual(status, None)
+        self.assertEqual(self.buffer.document().numLines(), 5)
+        self.assertEqual(self.buffer.document().lineMeta(1), {LineMeta.Change: "added"})
+        self.assertEqual(self.buffer.document().lineMeta(2), {})
+        self.assertEqual(self.buffer.documentCursor().pos(), (1,1))
+
+        command.undo()
+        self.assertEqual(self.buffer.document().numLines(), 4)
+        self.assertEqual(self.buffer.document().lineMeta(1), {})
+        self.assertEqual(self.buffer.documentCursor().pos(), (1,1))
+
 
     def testNewLineAfterCommand(self):
         command = commands.NewLineAfterCommand(self.buffer)
