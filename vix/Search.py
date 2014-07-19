4b0fd993ba41b9ebf6d884239e48f6df702c6d37
Stefano Borini <stefano.borini@gmail.com>
2014-07-19 12:17:51 +0200

    Added failing tests for search.

--- vix/Search.py
+++ vix/Search.py
@@ -1,22 +1,31 @@
-def find(buffer, text):
+def find(buffer, text, backwards=False):
     document = buffer.document()
     cursor = buffer.documentCursor()
 
     pos = cursor.pos()
 
     # -1 for indexing, +1 for skip current position
-    starting_column = pos[1]
-    for line_num in range(pos[0], document.numLines()+1):
-        index = document.lineText(line_num).find(text, starting_column)
+
+    if backwards is False:
+        intervals = [(pos[0], document.numLines()+1), (1, pos[0])]
+        starting_column = pos[1]
+        ending_column = None
+    else:
+        intervals = [(pos[0], 0, -1), (document.numLines(), pos[0]-1, -1)]
+        starting_column = None
+        ending_column = pos[1]-1
+
+
+    for line_num in range(*intervals[0]):
+        index = document.lineText(line_num).find(text, starting_column, ending_column)
         if index != -1:
             cursor.toPos((line_num, index+1))
             return True
 
         starting_column = 0
 
-
-    for line_num in range(1, pos[0]):
-        index = document.lineText(line_num).find(text, starting_column)
+    for line_num in range(*intervals[1]):
+        index = document.lineText(line_num).find(text)
         if index != -1:
             cursor.toPos((line_num, index+1))
             return True
