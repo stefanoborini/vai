56106106d34bdcea3d6011fbe889b4289fc0331f
Stefano Borini <stefano.borini@gmail.com>
2014-07-19 14:05:43 +0200

    Fixed test

--- tests/vix/test_Search.py
+++ tests/vix/test_Search.py
@@ -1,13 +1,5 @@
 import unittest
-from unittest.mock import Mock
-from vixtk import gui, test, core
-from vix.EditAreaController import EditAreaController
-from vix.EditArea import EditArea
-from vix.models.EditorModel import EditorModel
-from vix.models.Buffer import Buffer
-from vixtk.gui import events
 from .. import fixtures
-import vixtk
 from vix import flags
 from vix import Search
 
@@ -15,13 +7,13 @@ class TestSearch(unittest.TestCase):
     def testSearch(self):
         buffer = fixtures.buffer("real_case_editareacontroller.py")
         self.assertEqual(buffer.documentCursor().pos(), (1,1))
-        Search.find(buffer, 'Key')
+        Search.find(buffer, 'Key', direction=flags.FORWARD)
         self.assertEqual(buffer.documentCursor().pos(), (8,28))
-        Search.find(buffer, 'Key')
+        Search.find(buffer, 'Key', direction=flags.FORWARD)
         self.assertEqual(buffer.documentCursor().pos(), (8,32))
-        Search.find(buffer, 'Key')
+        Search.find(buffer, 'Key', direction=flags.FORWARD)
         self.assertEqual(buffer.documentCursor().pos(), (9,28))
-        Search.find(buffer, 'Key', backwards=True)
+        Search.find(buffer, 'Key', direction=flags.BACKWARD)
         self.assertEqual(buffer.documentCursor().pos(), (8,32))
 
 
