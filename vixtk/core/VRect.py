32933ce221051153df61ae0b4c3d562b6b31a979
Stefano Borini <stefano.borini@gmail.com>
2014-07-22 18:53:51 +0200

    Changed approach to get properties

--- vixtk/core/VRect.py
+++ vixtk/core/VRect.py
@@ -24,10 +24,13 @@ class VRect(object):
         else:
             self._size = VSize(width=size[0], height=size[1])
 
+    @property
     def size(self):
         return self._size
+    @property
     def height(self):
         return self._size.height()
+    @property
     def width(self):
         return self._size.width()
     def isNull(self):
@@ -40,63 +43,90 @@ class VRect(object):
                 and self.top() <= other.bottom()
                 and self.bottom() >= other.top())
 
+    @property
     def x(self):
-        return self.topLeft().x()
+        return self.topLeft().x
+    @property
     def y(self):
-        return self.topLeft().y()
+        return self.topLeft().y
+    @property
     def left(self):
         return self.x()
+    @property
     def right(self):
         return self.left() + self.width() - 1
+    @property
     def top(self):
         return self.y()
+    @property
     def bottom(self):
         return self.top() + self.height() - 1
+    @property
     def topLeft(self):
         return self._top_left
+    @property
     def topRight(self):
         return VPoint(self.right(), self.top())
+    @property
     def bottomLeft(self):
         return VPoint(self.left(), self.bottom())
+    @property
     def bottomRight(self):
         return VPoint(self.right(), self.bottom())
 
     def __str__(self):
         return "VRect(x=%d, y=%d, width=%d, height=%d)" % (self.x(), self.y(), self.width(), self.height())
 
-def x(rect):
-    return rect[0]
-def y(rect):
-    return rect[1]
-def left(rect):
-    return rect[0]
-def right(rect):
-    return rect[0] + rect[2] - 1
-def top(rect):
-    return rect[1]
-def bottom(rect):
-    return rect[1] + rect[3] - 1
-def topLeft(rect):
-    return (rect[0], rect[1])
-def topRight(rect):
-    return (rect[0] + rect[2] - 1, rect[1])
-def bottomLeft(rect):
-    return (rect[0], rect[1] + rect[3] - 1)
-def bottomRight(rect):
-    return (rect[0] + rect[2] - 1, rect[1] + rect[3] - 1)
-def size(rect):
-    return (rect[2], rect[3])
-def height(rect):
-    return rect[3]
-def width(rect):
-    return rect[2]
-def isNull(rect):
-    return (rect[2] == 0 and rect[3] == 0)
-def moveTo(rect, top_left):
-    return (top_left[0], top_left[1], rect[2], rect[3])
-def intersects(rect, other):
-    return (left(rect) <= right(other)
-                and right(rect) >= left(other)
-                and top(rect) <= bottom(other)
-                and bottom(rect) >= top(other))
+    class tuple:
+        @staticmethod
+        def x(rect):
+            return rect[0]
+        @staticmethod
+        def y(rect):
+            return rect[1]
+        @staticmethod
+        def left(rect):
+            return rect[0]
+        @staticmethod
+        def right(rect):
+            return rect[0] + rect[2] - 1
+        @staticmethod
+        def top(rect):
+            return rect[1]
+        @staticmethod
+        def bottom(rect):
+            return rect[1] + rect[3] - 1
+        @staticmethod
+        def topLeft(rect):
+            return (rect[0], rect[1])
+        @staticmethod
+        def topRight(rect):
+            return (rect[0] + rect[2] - 1, rect[1])
+        @staticmethod
+        def bottomLeft(rect):
+            return (rect[0], rect[1] + rect[3] - 1)
+        @staticmethod
+        def bottomRight(rect):
+            return (rect[0] + rect[2] - 1, rect[1] + rect[3] - 1)
+        @staticmethod
+        def size(rect):
+            return (rect[2], rect[3])
+        @staticmethod
+        def height(rect):
+            return rect[3]
+        @staticmethod
+        def width(rect):
+            return rect[2]
+        @staticmethod
+        def isNull(rect):
+            return (rect[2] == 0 and rect[3] == 0)
+        @staticmethod
+        def moveTo(rect, top_left):
+            return (top_left[0], top_left[1], rect[2], rect[3])
+        @staticmethod
+        def intersects(rect, other):
+            return (left(rect) <= right(other)
+                        and right(rect) >= left(other)
+                        and top(rect) <= bottom(other)
+                        and bottom(rect) >= top(other))
 
