7be28cac2203536ee97ba04a94194e8d6e913d92
Stefano Borini <stefano.borini@quantumwise.com>
2014-07-22 18:53:51 +0200

    Changed approach to get properties

--- tests/vixtk/core/test_VPoint.py
+++ tests/vixtk/core/test_VPoint.py
@@ -4,26 +4,26 @@ from vixtk import core
 class testVPoint(unittest.TestCase):
     def testVPoint(self):
         v = core.VPoint(x=4, y=5)
-        self.assertEqual(v.x(), 4)
-        self.assertEqual(v.y(), 5)
+        self.assertEqual(v.x, 4)
+        self.assertEqual(v.y, 5)
         self.assertEqual(tuple(v), (4,5))
 
     def testSum(self):
         v1 = core.VPoint(x=4, y=5)
         v2 = core.VPoint(x=2, y=3)
         vres = v1+v2
-        self.assertEqual(vres.x(), 6)
-        self.assertEqual(vres.y(), 8)
+        self.assertEqual(vres.x, 6)
+        self.assertEqual(vres.y, 8)
 
     def testDifference(self):
         v1 = core.VPoint(x=4, y=5)
         v2 = core.VPoint(x=2, y=2)
         vres = v1-v2
-        self.assertEqual(vres.x(), 2)
-        self.assertEqual(vres.y(), 3)
+        self.assertEqual(vres.x, 2)
+        self.assertEqual(vres.y, 3)
 
     def testVPointTuple(self):
-        self.assertEqual(core.VPoint.x((3,2)), 3)
-        self.assertEqual(core.VPoint.y((3,2)), 2)
-        self.assertEqual(core.VPoint.add((4,5), (2,3)), (6,8))
-        self.assertEqual(core.VPoint.sub((4,5), (2,2)), (2,3))
+        self.assertEqual(core.VPoint.tuple.x((3,2)), 3)
+        self.assertEqual(core.VPoint.tuple.y((3,2)), 2)
+        self.assertEqual(core.VPoint.tuple.add((4,5), (2,3)), (6,8))
+        self.assertEqual(core.VPoint.tuple.sub((4,5), (2,2)), (2,3))
--- vixtk/core/VPoint.py
+++ vixtk/core/VPoint.py
@@ -1,34 +1,41 @@
 class VPoint(object):
     def __init__(self, x, y):
-        self._pos = (x,y)
+        self._pos = (x, y)
 
     def __iter__(self):
         return iter(self._pos)
 
+    @property
     def x(self):
-        if isinstance(self, tuple):
-            return self[0]
-        else:
-            return self._pos[0]
+        return self._pos[0]
 
+    @property
     def y(self):
-        if isinstance(self, tuple):
-            return self[1]
-        else:
-            return self._pos[1]
+        return self._pos[1]
 
     def __add__(self, other):
-        return VPoint(self._pos[0]+other._pos[0], self._pos[1]+other._pos[1])
+        return VPoint(*VPoint.tuple.add(self._pos, other._pos))
 
     def __sub__(self, other):
-        return VPoint(self._pos[0]-other._pos[0], self._pos[1]-other._pos[1])
+        return VPoint(*VPoint.tuple.sub(self._pos, other._pos))
 
     def __str__(self):
         return "VPoint(x=%d, y=%d)" % self._pos
 
-    @staticmethod
-    def add(p1, p2):
-        return (p1[0]+p2[0], p1[1]+p2[1])
-    @staticmethod
-    def sub(p1, p2):
-        return (p1[0]-p2[0], p1[1]-p2[1])
+    class tuple:
+
+        @staticmethod
+        def x(p1):
+            return p1[0]
+
+        @staticmethod
+        def y(p1):
+            return p1[1]
+
+        @staticmethod
+        def add(p1, p2):
+            return (p1[0]+p2[0], p1[1]+p2[1])
+
+        @staticmethod
+        def sub(p1, p2):
+            return (p1[0]-p2[0], p1[1]-p2[1])
