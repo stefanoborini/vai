e2b8bfd8300dc540ca293bfefe836b026915d0f3
Stefano Borini <stefano.borini@gmail.com>
2014-07-22 20:02:00 +0200

    24: Added tuple methods to all

--- tests/vixtk/core/test_VRect.py
+++ tests/vixtk/core/test_VRect.py
@@ -4,8 +4,8 @@ from vixtk import core
 class TestVRect(unittest.TestCase):
     def testVRect(self):
         r = core.VRect((2,3), (4,5))
-        self.assertIsInstance(r.size(), core.VSize)
-        self.assertIsInstance(r.topLeft(), core.VPoint)
+        self.assertIsInstance(r.size, core.VSize)
+        self.assertIsInstance(r.top_left, core.VPoint)
 
     def testVRectIsNull(self):
         r = core.VRect((2,3), (4,5))
@@ -25,26 +25,33 @@ class TestVRect(unittest.TestCase):
         #self.assertFalse(core.VRect((2,3), (1,1)).intersects(core.VRect((4,4), (1,1))))
 
         self.assertTrue(core.VRect((0,0), (18,1)).intersects(core.VRect((4,0), (142,40))))
+
     def testVRectDimensionality(self):
         r = core.VRect((2,3), (4,5))
 
-        self.assertEqual(r.x(), 2)
-        self.assertEqual(r.y(), 3)
-        self.assertEqual(r.width(), 4)
-        self.assertEqual(r.height(), 5)
-        self.assertEqual(r.size().width(), 4)
-        self.assertEqual(r.size().height(), 5)
-        self.assertEqual(r.topLeft().x, 2)
-        self.assertEqual(r.topLeft().y, 3)
-        self.assertEqual(r.topRight().x, 5)
-        self.assertEqual(r.topRight().y, 3)
-        self.assertEqual(r.bottomLeft().x, 2)
-        self.assertEqual(r.bottomLeft().y, 7)
-        self.assertEqual(r.bottomRight().x, 5)
-        self.assertEqual(r.bottomRight().y, 7)
-        self.assertEqual(r.left(), 2)
-        self.assertEqual(r.right(), 5)
-        self.assertEqual(r.top(), 3)
-        self.assertEqual(r.bottom(), 7)
+        self.assertEqual(r.x, 2)
+        self.assertEqual(r.y, 3)
+        self.assertEqual(r.width, 4)
+        self.assertEqual(r.height, 5)
+        self.assertEqual(r.size.width, 4)
+        self.assertEqual(r.size.height, 5)
+        self.assertEqual(r.top_left.x, 2)
+        self.assertEqual(r.top_left.y, 3)
+        self.assertEqual(r.top_right.x, 5)
+        self.assertEqual(r.top_right.y, 3)
+        self.assertEqual(r.bottom_left.x, 2)
+        self.assertEqual(r.bottom_left.y, 7)
+        self.assertEqual(r.bottom_right.x, 5)
+        self.assertEqual(r.bottom_right.y, 7)
+        self.assertEqual(r.left, 2)
+        self.assertEqual(r.right, 5)
+        self.assertEqual(r.top, 3)
+        self.assertEqual(r.bottom, 7)
 
+    def testVRectTuple(self):
+        self.assertEqual(core.VRect.tuple.x((2,3,4,5)), 2)
+        self.assertEqual(core.VRect.tuple.y((2,3,4,5)), 3)
+        self.assertEqual(core.VRect.tuple.width((2,3,4,5)), 4)
+        self.assertEqual(core.VRect.tuple.height((2,3,4,5)), 5)
+        
 
--- vixtk/core/VRect.py
+++ vixtk/core/VRect.py
@@ -29,53 +29,56 @@ class VRect(object):
         return self._size
     @property
     def height(self):
-        return self._size.height()
+        return self._size.height
     @property
     def width(self):
-        return self._size.width()
+        return self._size.width
+
     def isNull(self):
-        return (self.width() == 0 and self.height() == 0)
+        return (self.width == 0 and self.height == 0)
+
     def moveTo(self, top_left):
         self._top_left = top_left
+
     def intersects(self, other):
-        return (self.left() <= other.right()
-                and self.right() >= other.left()
-                and self.top() <= other.bottom()
-                and self.bottom() >= other.top())
+        return (self.left <= other.right
+                and self.right >= other.left
+                and self.top <= other.bottom
+                and self.bottom >= other.top)
 
     @property
     def x(self):
-        return self.topLeft().x
+        return self.top_left.x
     @property
     def y(self):
-        return self.topLeft().y
+        return self.top_left.y
     @property
     def left(self):
-        return self.x()
+        return self.x
     @property
     def right(self):
-        return self.left() + self.width() - 1
+        return self.left + self.width - 1
     @property
     def top(self):
-        return self.y()
+        return self.y
     @property
     def bottom(self):
-        return self.top() + self.height() - 1
+        return self.top + self.height - 1
     @property
-    def topLeft(self):
+    def top_left(self):
         return self._top_left
     @property
-    def topRight(self):
-        return VPoint(self.right(), self.top())
+    def top_right(self):
+        return VPoint(self.right, self.top)
     @property
-    def bottomLeft(self):
-        return VPoint(self.left(), self.bottom())
+    def bottom_left(self):
+        return VPoint(self.left, self.bottom)
     @property
-    def bottomRight(self):
-        return VPoint(self.right(), self.bottom())
+    def bottom_right(self):
+        return VPoint(self.right, self.bottom)
 
     def __str__(self):
-        return "VRect(x=%d, y=%d, width=%d, height=%d)" % (self.x(), self.y(), self.width(), self.height())
+        return "VRect(x=%d, y=%d, width=%d, height=%d)" % (self.x, self.y, self.width, self.height)
 
     class tuple:
         @staticmethod
@@ -125,8 +128,8 @@ class VRect(object):
             return (top_left[0], top_left[1], rect[2], rect[3])
         @staticmethod
         def intersects(rect, other):
-            return (left(rect) <= right(other)
-                        and right(rect) >= left(other)
-                        and top(rect) <= bottom(other)
-                        and bottom(rect) >= top(other))
+            return (VRect.tuple.left(rect) <= VRect.tuple.right(other)
+                    and VRect.tuple.right(rect) >= VRect.tuple.left(other)
+                    and VRect.tuple.top(rect) <= VRect.tuple.bottom(other)
+                    and VRect.tuple.bottom(rect) >= VRect.tuple.top(other))
 
