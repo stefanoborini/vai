0d32422327e06da1aaff83130e02e9b3bb92cda6
Stefano Borini <stefano.borini@gmail.com>
2014-07-23 20:06:08 +0200

    Closes #50: Find all occurrences

--- tests/vix/test_Search.py
+++ tests/vix/test_Search.py
@@ -16,7 +16,19 @@ class TestSearch(unittest.TestCase):
         Search.find(buffer, 'Key', direction=flags.BACKWARD)
         self.assertEqual(buffer.documentCursor().pos(), (8,32))
 
+    def testFindAll(self):
+        buffer = fixtures.buffer("real_case_editareacontroller.py")
+        doc = buffer.document()
+        all_finds = Search.findAll(doc, 'Key')
+        self.assertEqual(len(all_finds), 65)
+        self.assertEqual(all_finds[0], (8, 28, 31))
+        self.assertEqual(all_finds[-1], (194, 37, 40))
 
+        all_finds = Search.findAll(doc, 'key_')
+        self.assertEqual(len(all_finds), 0)
+
+        all_finds = Search.findAll(doc, 'key_', case_sensitive=False)
+        self.assertNotEqual(len(all_finds), 0)
 
 if __name__ == '__main__':
     unittest.main()
--- vix/Search.py
+++ vix/Search.py
@@ -1,4 +1,27 @@
+import re
 from . import flags
+
+def findAll(document, search_text, line_interval=None, case_sensitive=True):
+    """
+    Find all occurrences of a given search text (evt. regexp text)
+    """
+    match_pos = []
+    if line_interval is None:
+        line_interval = (1, document.numLines()+1)
+    else:
+        line_interval = (max(1, line_interval[0]), min(document.numLines()+1, line_interval[1]))
+
+    flags = 0
+    if not case_sensitive:
+       flags = re.IGNORECASE
+
+    allMatches = re.compile(re.escape(search_text), flags).finditer
+
+    for line_num in range(*line_interval):
+        line_text = document.lineText(line_num)
+        match_pos.extend( ((line_num, m.start()+1, m.end()+1) for m in allMatches(line_text)) )
+
+    return match_pos
 
 def find(buffer, text, direction):
     document = buffer.document()
