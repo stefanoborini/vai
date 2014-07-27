67107ef9c6b5134106ac006abc70b84eb9601ad2
Stefano Borini <stefano.borini@gmail.com>
2014-07-27 20:11:51 +0200

    Added symbol database and lookup

---
+++ vix/SymbolLookupDb.py
@@ -0,0 +1,44 @@
+class SymbolLookupDb:
+    def __init__(self):
+        self._db = {}
+    
+    def add(self, word):
+        _add(self._db, word)
+    
+    def lookup(self, prefix):
+        d = _walkDown(self._db, prefix)    
+        if d is None:
+            return []
+        
+        ret = _composePostfix(d)
+        return ret
+            
+def _add(d, word):
+    if len(word) == 0:
+        d[''] = None
+        return
+    if word[0] not in d:
+        d[word[0]] = {}
+    
+    _add(d[word[0]], word[1:])
+
+def _walkDown(d, prefix):
+    if len(prefix) == 1:
+        return d[prefix[0]]
+    
+    if prefix[0] not in d:
+        return None
+        
+    return _walkDown(d[prefix[0]], prefix[1:])
+    
+def _composePostfix(d, tab=0):
+    ret = []
+    if d is None:
+        return ['']
+
+    for k, v in d.items():
+        for postfix in _composePostfix(v, tab+1):
+            ret.append(k+postfix)
+            print(ret)
+    
+    return ret    
