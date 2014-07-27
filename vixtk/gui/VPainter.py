c32b37182e72faa08dd096b3a751b4f959320672
Stefano Borini <stefano.borini@gmail.com>
2014-07-27 13:38:45 +0200

    VPainter text alignment

--- vixtk/gui/VPainter.py
+++ vixtk/gui/VPainter.py
@@ -1,4 +1,4 @@
-from .. import Orientation, LineCapStyle, LineStyle, CornerCapStyle
+from .. import Orientation, LineCapStyle, LineStyle, CornerCapStyle, Alignment
 from ..consts import Index
 
 
@@ -25,10 +25,60 @@ class VPainter:
     def bg_color(self, color):
         self._bg_color = color
 
-    def drawText(self, pos, string):
-        self._widget.screenArea().write(pos, string, self.fg_color, self.bg_color)
+    def drawText(self, pos_or_rect, string, align=0):
+        if len(pos_or_rect) == 2:
+            self._widget.screenArea().write(pos_or_rect, string, self.fg_color, self.bg_color)
+            return
+
+        if align & (Alignment.AlignTop | Alignment.AlignVCenter | Alignment.AlignBottom) == 0:
+            align |= Alignment.AlignTop
+
+        if align & (Alignment.AlignLeft | Alignment.AlignHCenter | Alignment.AlignRight) == 0:
+            align |= Alignment.AlignLeft
+
+        rect = pos_or_rect
+
+        if align & Alignment.AlignLeft:
+            line_formatted = string.ljust(rect[Index.RECT_WIDTH])
+        elif align & Alignment.AlignRight:
+            line_formatted = string.rjust(rect[Index.RECT_WIDTH])
+        elif align & Alignment.AlignHCenter:
+            line_formatted = string.center(rect[Index.RECT_WIDTH])
+        else:
+            line_formatted = string.ljust(rect[Index.RECT_WIDTH])
+
+        for h in range(rect[Index.RECT_HEIGHT]):
+            if (align & Alignment.AlignTop and h == 0) or \
+               (align & Alignment.AlignVCenter and h == int(rect[Index.RECT_HEIGHT]/2)) or\
+               (align & Alignment.AlignBottom and h == rect[Index.RECT_HEIGHT]-1):
+
+                self._widget.screenArea().write( (rect[Index.RECT_X], rect[Index.RECT_Y]+h),
+                                                      line_formatted,
+                                                      self.fg_color,
+                                                      self.bg_color)
+
+            else:
+                self._widget.screenArea().write( (rect[Index.RECT_X], rect[Index.RECT_Y]+h),
+                                                  ' '*rect[Index.RECT_WIDTH],
+                                                  self.fg_color,
+                                                  self.bg_color)
 
     def drawRect(self, rect):
+        screen_area = self._widget.screenArea()
+
+        x, y = rect[Index.RECT_X], rect[Index.RECT_Y]
+        w, h = rect[Index.RECT_WIDTH], rect[Index.RECT_HEIGHT]
+        fg, bg = self.fg_color, self.bg_color
+        cap, line, vline = _getRectComponents(self._corner_cap_style, self._line_style)
+
+        if w >= 2 and h >= 2:
+            screen_area.write( (x,y), cap + line * (w-2) + cap, fg, bg)
+            for i in range(0, h-2):
+                screen_area.write( (x,y+i+1), vline, fg, bg)
+                screen_area.write( (x+w-1, y+i+1), vline, fg, bg)
+            screen_area.write( (x, y+h-1), cap+line*(w-2)+cap, fg, bg)
+
+    def fillRect(self, rect):
         screen_area = self._widget.screenArea()
 
         x, y = rect[Index.RECT_X], rect[Index.RECT_Y]
@@ -98,3 +148,6 @@ def _getRectComponents(corner_cap_style, line_style):
           }.get(corner_cap_style, ' ')
 
     return (cap, line, vline)
+
+
+
