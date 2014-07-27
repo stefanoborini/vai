3ed4ed13842dbfc552280280591f840062ce1538
Stefano Borini <stefano.borini@gmail.com>
2014-07-27 15:36:49 +0200

    Closes #73: hovering tooltip

--- vixtk/gui/widgets/VToolTip.py
+++ vixtk/gui/widgets/VToolTip.py
@@ -1,27 +1,29 @@
 from .VLabel import VLabel
 from ..VPainter import VPainter
 from ..VPalette import VPalette
-from ... import core
 
 class VToolTip(VLabel):
-    @staticmethod
-    def showText(pos, text):
-        tip = VToolTip(text, parent=None)
-        tip.resize(tip.minimumSize())
-        tip.move(pos)
-        tip.show()
+    _instance = None
+    @classmethod
+    def showText(cls, pos, text):
+        if cls._instance is None:
+            tip = VToolTip(text, parent=None)
+            tip.resize(tip.minimumSize())
+            tip.move(pos)
+            tip.show()
+            cls._instance = tip
+        return
+    @classmethod
+    def hide(cls):
+        if cls._instance is not None:
+            VLabel.hide(cls._instance)
+            cls._instance = None
 
     def paintEvent(self, event):
         painter = VPainter(self)
         w, h = self.size()
-        fg_color = self.palette().color(VPalette.ColorGroup.Active,
+        painter.fg_color = self.palette().color(VPalette.ColorGroup.Active,
                                         VPalette.ColorRole.ToolTipText)
-        bg_color = self.palette().color(VPalette.ColorGroup.Active,
+        painter.bg_color = self.palette().color(VPalette.ColorGroup.Active,
                                         VPalette.ColorRole.ToolTipBase)
-        for i in range(0, int(h/2)):
-            painter.drawText( core.VPoint(0, i), ' '*w, fg_color, bg_color)
-        painter.drawText( core.VPoint(0, int(h/2)), self._label + ' '*(w-len(self._label)), fg_color, bg_color)
-        for i in range(1+int(h/2), h):
-            painter.drawText( core.VPoint(0, i), ' '*w, fg_color, bg_color)
-
-
+        painter.drawText( (0, 0), self._label)
