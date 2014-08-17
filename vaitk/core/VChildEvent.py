9c45c1f64bf5ba037752381a587872ce1155c6f0
Stefano Borini <stefano.borini@gmail.com>
2014-08-17 22:47:51 +0200

    Minor addition to childevent

--- vaitk/core/VChildEvent.py
+++ vaitk/core/VChildEvent.py
@@ -2,6 +2,12 @@ from .VEvent import VEvent
 
 class VChildEvent(VEvent):
     def __init__(self, event_type, child):
+        if event_type not in [VEvent.EventType.ChildAdded,
+                              VEvent.EventType.ChildPolished,
+                              VEvent.EventType.ChildRemoved]:
+            raise Exception("Invalid child event %s" % str(event_type))
         super().__init__(event_type)
         self._child = child
 
+    def child(self):
+        return self._child
