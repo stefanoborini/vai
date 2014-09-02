4eb914625ffa5ee57b14ded2aaf6e09f4bfc90dd
Stefano Borini <stefano.borini@gmail.com>
2014-09-02 23:03:03 +0200

    Closes #120

--- vai/commands/DeleteToEndOfWordCommand.py
+++ vai/commands/DeleteToEndOfWordCommand.py
@@ -18,16 +18,23 @@ class DeleteToEndOfWordCommand(BufferCommand):
             document.updateLineMeta(pos[0], {LineMeta.Change: "modified"})
 
         text = document.lineText(pos[0])
-        if text[pos[1]-1] in DeleteToEndOfWordCommand.SPACERS:
-            remove_count = len(text[pos[1]-1:]) - len(text[pos[1]-1:].lstrip(DeleteToEndOfWordCommand.SPACERS))
+
+        # Zero based, for text.
+        cur_index = pos[1]-1
+
+        if text[cur_index] in DeleteToEndOfWordCommand.SPACERS:
+            # cursor is on a spacer. Delete everything up to the first non spacer
+            remove_count = len(text[cur_index:]) - len(text[cur_index:].lstrip(DeleteToEndOfWordCommand.SPACERS))
         else:
             # Get the next spacer from the current position.
             # It's the first of this list comprehension, if available
-            spacer_indexes = [ p for p, c in enumerate(text) if (p > pos[1]-1 and c in DeleteToEndOfWordCommand.SPACERS)]
+            spacer_indexes = [ p for p, c in enumerate(text) if (p > cur_index and c in DeleteToEndOfWordCommand.SPACERS)]
             if len(spacer_indexes) == 0:
-                remove_count = document.lineLength(pos[0])-pos[1]
+                remove_count = document.lineLength(pos[0]) - cur_index + 1
             else:
-                remove_count = (spacer_indexes[0]+1) - pos[1]
+                # Remove the non-spacer, plus all the spacers up to the next non-spacer
+                remove_count = spacer_indexes[0] - cur_index
+                remove_count += len(text[cur_index+remove_count:]) - len(text[cur_index+remove_count:].lstrip(DeleteToEndOfWordCommand.SPACERS))
 
         deleted = document.deleteChars(pos, remove_count)
         return CommandResult(success=True, info=deleted)
