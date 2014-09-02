b439cce3ad2e13de92ae784a6a49d4b21edd7424
Stefano Borini <stefano.borini@gmail.com>
2014-09-02 19:25:02 +0200

    Fixed #121

--- tests/vai/test_EditAreaController.py
+++ tests/vai/test_EditAreaController.py
@@ -1,37 +1,39 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, PropertyMock
 from vaitk import gui, test, core
-from vai.controllers import EditAreaController
 from vai.EditArea import EditArea
-from vai.models import GlobalState
-from vai.models import Buffer
+from vai import models
 from vaitk.gui import events
 import vaitk
 from vai import flags
+from tests import fixtures
+
+from vai import controllers
+
 
 class TestEditAreaController(unittest.TestCase):
     def setUp(self):
         self.mock_edit_area = Mock(spec=EditArea)
-        self.mock_buffer = Mock(spec=Buffer)
-        self.mock_editor_model = Mock(spec=EditorModel)
-        self.mock_editor_model.mode.return_value = flags.COMMAND_MODE
+        self.mock_edit_area.height.return_value = 20
+        self.mock_edit_area.width.return_value = 20
+        self.mock_buffer = Mock(spec=models.Buffer)
+        self.mock_global_state = Mock(spec=models.GlobalState)
+        type(self.mock_global_state).editor_mode = PropertyMock(return_value=models.EditorMode.COMMAND)
+        self.mock_editor_controller = Mock(spec=controllers.EditorController)
 
-    @unittest.skip("Need to mock property")
-    def testEditAreaController(self):
-        controller = EditAreaController(self.mock_edit_area)
-        controller.setModels(self.mock_buffer, self.mock_editor_model)
-        event = events.VKeyEvent(vaitk.Key.Key_D)
+    def testBug121(self):
+        buffer = fixtures.buffer("basic_python.py")
+        controller = controllers.EditAreaController(self.mock_edit_area,
+                                                    self.mock_global_state,
+                                                    self.mock_editor_controller)
+        controller.buffer = buffer
+        buffer.cursor.toPos((1,3))
+
+        line_before = buffer.document.lineText(1)
+        event = events.VKeyEvent(vaitk.Key.Key_Backspace)
         controller.handleKeyEvent(event)
-        self.assertTrue(self.mock_editor_model.mode.__set__.called)
-        self.assertEqual(self.mock_editor_model.mode.call_args[0][0], flags.DELETE_MODE)
-
-
-        self.mock_editor_model.mode.reset_mock()
-        self.mock_editor_model.mode.return_value = flags.DELETE_MODE
-        event = events.VKeyEvent(vaitk.Key.Key_Q)
-        controller.handleKeyEvent(event)
-        self.assertTrue(self.mock_editor_model.mode.__set__.called)
-        self.assertEqual(self.mock_editor_model.mode.call_args[0][0], flags.COMMAND_MODE)
+        self.assertEqual(buffer.cursor.pos, (1,2))
+        self.assertEqual(line_before, buffer.document.lineText(1))
 
 
 if __name__ == '__main__':
