01f951327910f8f829f996e324cbb545cb846f62
Stefano Borini <stefano.borini@gmail.com>
2014-11-09 13:52:12 +0100

    DeleteSingleCharCommand and JoinWithNextLineCommand

---
+++ tests/vai/commands/test_JoinWithNextLineCommand.py
@@ -0,0 +1,35 @@
+import unittest
+from vai.models.Buffer import Buffer
+from vai.models.TextDocument import TextDocument
+from vai.models.EditAreaModel import EditAreaModel
+from vai import commands
+from tests import fixtures
+
+class TestJoinWithNextLineCommand(unittest.TestCase):
+    def setUp(self):
+        self.buffer = Buffer()
+        self.buffer.document.open(fixtures.get("basic_python.py"))
+
+    def testJoinWithNextLineCommand(self):
+        line_1 = self.buffer.document.lineText(1)
+        line_2 = self.buffer.document.lineText(2)
+        cursor = self.buffer.cursor
+        cursor.toPos((1,1))
+
+        command = commands.JoinWithNextLineCommand(self.buffer)
+        result = command.execute()
+        self.assertTrue(result.success)
+        self.assertEqual(self.buffer.document.lineText(1), line_1[:-1]+line_2)
+        self.assertEqual(cursor.pos, (1,1))
+        self.assertEqual(self.buffer.document.numLines(), 3)
+
+    def testJoinWithNextLineCommand2(self):
+        self.buffer.cursor.toLastLine()
+        command = commands.JoinWithNextLineCommand(self.buffer)
+        result = command.execute()
+        self.assertFalse(result.success)
+
+        # FIXME test undo
+
+if __name__ == '__main__':
+    unittest.main()
