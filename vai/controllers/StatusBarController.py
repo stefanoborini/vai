78aa5e070e29e7070cb55b70d86b3be2a217d7e8
Stefano Borini <stefano.borini@gmail.com>
2014-11-09 21:53:00 +0100

    Moved the color name to color to vaitk

--- vai/controllers/StatusBarController.py
+++ vai/controllers/StatusBarController.py
@@ -1,12 +1,12 @@
 from ..models import Configuration
-from ..Utils import stringToColor
+from vaitk import gui
 
 class StatusBarController(object):
     def __init__(self, status_bar):
         self._status_bar = status_bar
         config = Configuration.instance()
-        self._status_bar.setColors(stringToColor(config["colors.status_bar.fg"]),
-                                   stringToColor(config["colors.status_bar.bg"])
+        self._status_bar.setColors(gui.VGlobalColor.nameToColor(config["colors.status_bar.fg"]),
+                                   gui.VGlobalColor.nameToColor(config["colors.status_bar.bg"])
                                   )
         self._buffer = None
 
