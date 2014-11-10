e6d233082f7d26f9e1c31295be452688cbc19432
Stefano Borini <stefano.borini@gmail.com>
2014-11-10 23:44:24 +0100

    Slightly better performance for lexer selection

--- vai/Lexer.py
+++ vai/Lexer.py
@@ -4,6 +4,48 @@ from .SymbolLookupDb import SymbolLookupDb
 from .models.TextDocument import CharMeta
 import pygments.token
 from pygments import lexers, util
+import os
+
+# Faster lookup than the one provided in lexers.get_lexer_for_filename.
+# Any other attempt has been proved to take around 0.2 secs, which is
+# unacceptable. We need the fastest lookup to start as soon as possible
+EXTENSION_TO_LEXER = {
+    ".py" : lexers.PythonLexer,
+    ".rb" : lexers.RubyLexer,
+    ".pl" : lexers.PerlLexer,
+    ".tcl": lexers.TclLexer,
+    ".lua": lexers.LuaLexer,
+    ".as" : lexers.ActionScriptLexer,
+    ".c"  : lexers.CLexer,
+    ".h"  : lexers.CLexer,
+    ".cpp" : lexers.CppLexer,
+    ".hpp" : lexers.CppLexer,
+    ".f"   : lexers.FortranLexer,
+    ".f77" : lexers.FortranLexer,
+    ".f90" : lexers.FortranLexer,
+    ".sh"  : lexers.BashLexer,
+    ".bat" : lexers.BatchLexer,
+}
+
+def _getLexerInstance(filename):
+    """Get the lexer instance from the filename"""
+    if filename is None:
+        return lexers.TextLexer(stripnl=False, stripall=False)
+
+    # Fast lookup first
+    ext = os.path.splitext(filename)[1]
+    cls = EXTENSION_TO_LEXER.get(ext)
+    if cls is not None:
+        return cls(stripnl=False, stripall=False)
+
+    # Slow lookup for less common cases
+    try:
+        lexer = lexers.get_lexer_for_filename(filename, stripnl=False, stripall=False)
+    except util.ClassNotFound:
+        lexer = lexers.TextLexer(stripnl=False, stripall=False)
+
+    return lexer
+
 
 class Lexer:
     def __init__(self):
@@ -13,26 +55,14 @@ class Lexer:
     def setModel(self, document):
         self._document = document
         filename = self._document.filename()
-        if filename is None:
-            self._lexer = None
-        else:
-            try:
-                self._lexer = lexers.get_lexer_for_filename(self._document.filename(), stripnl=False, stripall=False)
-            except util.ClassNotFound:
-                self._lexer = None
-
+        self._lexer = _getLexerInstance(filename)
         self._document.contentChanged.connect(self._lexContents)
         self._lexContents()
 
     def _lexContents(self):
-
         if self._lexer is None:
             return
 
-        # We add a space in front because otherwise the lexer will discard
-        # everything up to the first token, meaning that we lose the potentially
-        # empty first lines and mess up the matching. With the space, we force
-        # the lexer to process the initial \n. and we just skip the space token
         tokens = self._lexer.get_tokens(self._document.documentText())
         current_line = 1
         current_col = 1
