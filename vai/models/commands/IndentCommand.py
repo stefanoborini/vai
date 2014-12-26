7c510505048e48037f2f1148c058869fb6598902
Stefano Borini <stefano.borini@gmail.com>
2014-12-26 13:57:12 +0100

    Forgit indentcommand file

---
+++ vai/models/commands/IndentCommand.py
@@ -0,0 +1,32 @@
+from .BufferCommand import BufferCommand
+from .CommandResult import CommandResult
+
+class IndentCommand(BufferCommand):
+    """
+    Indent the line at the current cursor position.
+    """
+    def __init__(self, buffer):
+        super().__init__(buffer)
+
+    def execute(self):
+        cursor = self._cursor
+        document = self._document
+
+        if self.savedCursorPos() is None:
+            self.saveCursorPos()
+
+        pos = self.savedCursorPos()
+
+        line_meta = document.lineMetaInfo("Change")
+        changed = line_meta.data(pos[0])
+
+        self.saveLineMemento(pos[0], BufferCommand.MEMENTO_REPLACE)
+
+        if changed is None:
+            line_meta.setData("modified", pos[0])
+
+        document.insertChars((pos[0], 1), " "*4)
+        cursor.toPos((pos[0], pos[1]+4))
+
+        return CommandResult(success=True, info=None)
+
