3155d2a418e16044b833f9ddfdcd96229a87fded
Stefano Borini <stefano.borini@gmail.com>
2015-01-11 18:22:19 +0100

    Refactored fixtures module

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,29 +1,58 @@
+"""
+Module to handle fixtures in the test system.
+It makes access to fixtures easy, convenient and practical.
+"""
 import inspect
 import os
 from vai.models import Buffer
-from vai.models import EditAreaModel
-from vai.models import TextDocument
 
+# The place where global fixtures are looked up if local fixtures are not found
 GLOBAL_FIXTURES_DIR = os.path.join(os.path.dirname(__file__), "global_fixtures")
 
 def get(name, dirname="fixtures"):
+    """
+    Method to get a specific fixture path given its filename
+    When invoked in a module, dir or method, the fixtures are searched
+    in the current module directory, under the "fixtures" subdir.
+    If that file does not exist, it will check the global fixtures.
+    """
+    fixture_local_dir = localDir(dirname)
+    path = os.path.join(fixture_local_dir, name)
+    if os.path.exists(path):
+        return path
+    
+    return os.path.join(GLOBAL_FIXTURES_DIR, name)
+
+def localDir(dirname="fixtures"):
+    """
+    Produces the local dir for the fixtures
+    """
     frames = inspect.getouterframes(inspect.currentframe())
     for frame_info in frames:
+        # Skips all routines inside this file, until you reach one that isn't
         if frame_info[0].f_globals['__file__'] != __file__:
             current_path = os.path.dirname(frame_info[1])
-            fixture_path = os.path.join(current_path, dirname, name)
-            if os.path.exists(fixture_path):
-                return fixture_path
-            else:
-                return os.path.join(GLOBAL_FIXTURES_DIR, name)
+            return os.path.join(current_path, dirname)
+
+    raise Exception("Should not reach")
+
 
 def buffer(name):
+    """
+    Creates and returns a fully initialized buffer from a given fixture name
+    """
     buf = Buffer()
     buf.document.open(get(name))
     return buf
 
 def tempFile(name):
-    path = get(name, "_test_")
+    """
+    Returns a temporary file in a proper subdirectory
+    Useful if the test needs a file that must be used and deleted afterwards
+    """
+    
+    path = os.path.join(localDir("_test_"), name)
+
     try:
         os.remove(path)
     except FileNotFoundError:
@@ -31,3 +60,4 @@ def tempFile(name):
     if not os.path.exists(os.path.dirname(path)):
         os.mkdir(os.path.dirname(path))
     return path
+
