ec5070325a4bb6507f9b9e2a427659554fff7889
Stefano Borini <stefano.borini@gmail.com>
2015-01-11 16:16:37 +0100

    Added tooltip timeout #163

--- vai/InfoHoverBox.py
+++ vai/InfoHoverBox.py
@@ -8,7 +8,13 @@ class InfoHoverBox(core.VObject):
 
     def __init__(self):
         self._buffer = None
+
+        # The line for which the popup is visible 
         self._current_shown_line = None
+        self._hide_timer = core.VTimer()
+        self._hide_timer.setInterval(2000)
+        self._hide_timer.setSingleShot(True)
+        self._hide_timer.timeout.connect(self._toolTipTimeOut)
 
     @property
     def buffer(self):
@@ -18,6 +24,9 @@ class InfoHoverBox(core.VObject):
     def buffer(self, buf):
         if buf is None:
             raise Exception("Buffer cannot be None")
+
+        if self._hide_timer.isRunning():
+            self._hide_timer.stop()
 
         if self._buffer is not None:
             self._buffer.cursor.positionChanged.disconnect(self._cursorPositionChanged)
@@ -30,17 +39,29 @@ class InfoHoverBox(core.VObject):
             return
 
         cursor = self._buffer.cursor
-        pos_at_top = self._buffer.edit_area_model.document_pos_at_top
 
+        # If the cursor changed but remained on the same line, let things be
         if self._current_shown_line == cursor.pos[0]:
-            gui.VToolTip.hide()
             return
 
+        # Line changed. Stop the timer and hide the tooltip.
+        self._hide_timer.stop()
+        gui.VToolTip.hideText()
+
+        # If there's information, show the new one
         lint = self._buffer.document.lineMetaInfo("LinterResult").data(cursor.pos[0])
 
         if lint is not None:
+            pos_at_top = self._buffer.edit_area_model.document_pos_at_top
             self._current_shown_line = cursor.pos[0]
             gui.VToolTip.showText((0, cursor.pos[0]-pos_at_top[0]+1), lint.message)
+            self._hide_timer.start()
         else:
             self._current_shown_line = None
-            gui.VToolTip.hideText()
+        
+    def _toolTipTimeOut(self):
+        """Callback to hide the tooltip after a few seconds"""
+        gui.VToolTip.hideText()
+
+
+
