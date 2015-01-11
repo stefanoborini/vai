651dc2828f39331dcbbb8610c67c5187304c20c5
Stefano Borini <stefano.borini@gmail.com>
2015-01-11 21:55:08 +0100

    Comment

--- vai/linting/LinterResult.py
+++ vai/linting/LinterResult.py
@@ -1,8 +1,12 @@
 class LinterResult:
+    """
+    "Namedtuple" class to hold information about the linter results
+    """
     class Level:
         INFO = 0
         WARNING = 1
         ERROR = 2
+
     def __init__(self, filename, level, line, column, message):
         self.filename = filename
         self.level = level
