fbb75df460b7e10f900380d080169794d596b9da
Stefano Borini <stefano.borini@gmail.com>
2015-02-05 18:24:13 +0100

    Added yapsy to do plugins. No need to reinvent the wheel.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
     test_suite = 'nose.collector',
-    install_requires = ['pygments==2.0.1', "pylint==1.3.0", 'pyflakes==0.8.1'],
+    install_requires = ['pygments==2.0.1', "pylint==1.3.0", 'pyflakes==0.8.1', 'yapsy==1.10.423'],
     entry_points={
         'console_scripts': [
             'vai_exec=vai:main',
